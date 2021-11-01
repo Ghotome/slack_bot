97e15ae030b822acf4110ebce4e2393660b633d2
fishhead <egor@nefedov.org>
2021-11-01 10:40:27 +0200

    123

--- init.py
+++ init.py
@@ -5,12 +5,12 @@ import subprocess
 
 def __init__():
     python = "#!/home/egor/projects/slackk_bot/slack_bot/bin/python3"
-    subprocess.run('python3 -m venv slack_bot;'
+    print(subprocess.Popen('python3 -m venv slack_bot;'
                      '. ./slack_bot/bin/activate;'
                      'pip install --upgrade pip;'
                      'pip install -r requirements.txt;'
                      'deactivate;'
-                     'chmod +x main.py', shell=True, stdout=subprocess.PIPE)
+                     'chmod +x main.py', shell=True, stdout=subprocess.PIPE).stdout.read())
     edited_file = "main.py.bak"
     main_file = "main.py"
     with open(main_file, 'r') as read_object, open(edited_file, 'w') as write_object:
