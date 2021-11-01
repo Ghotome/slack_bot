13346a522d50e86fd46a2c88f5f2c4245fe39638
fishhead <egor@nefedov.org>
2021-11-01 09:39:37 +0200

    123

--- init.py
+++ init.py
@@ -1,11 +1,12 @@
 import os
 
+
 def __init__():
     python = "#!/home/egor/projects/slackk_bot/slack_bot/bin/python3"
     os.system('python3 -m venv slack_bot | '
               'source ./slack_bot/bin/activate | '
               'pip install ---upgrade pip | '
-              'pip install -r requirements.txt.txt')
+              'pip install -r requirements.txt')
     edited_file = "main.py.bak"
     main_file = "main.py"
     with open(main_file, 'r') as read_object, open(edited_file, 'w') as write_object:
