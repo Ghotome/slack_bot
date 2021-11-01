0126ae230769f3a1f6e39b95c2ebeb8141116751
fishhead <egor@nefedov.org>
2021-11-01 11:39:43 +0200

    123

--- init.py
+++ init.py
@@ -19,7 +19,7 @@ def __init__():
 
         os.remove(main_file)
         os.rename(edited_file, main_file)
-    print(os.chmod('./main.py', 755))
+    print(os.chmod('./main.py', 555))
     os.system('./main.py')
 
 
