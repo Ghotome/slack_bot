d1a75c30c4857db3af7544accd2b1418179b44e4
fishhead <egor@nefedov.org>
2021-11-01 11:19:41 +0200

    123

--- init.py
+++ init.py
@@ -8,7 +8,7 @@ def __init__():
     print(subprocess.Popen(('python3 -m venv slack_bot && '
                      '. ~/projects/slackk_bot/slack_bot/bin/activate && '
                      'pip install --upgrade pip && '
-                     'pip install -r requirements.txt && '
+                     'pip install -r requirements.txt; '
                      'chmod +x ~/projects/slackk_bot/main.py'), shell=True,
                            stdout=subprocess.PIPE, env=os.environ).stdout.read().decode('utf-8'))
     edited_file = "main.py.bak"
