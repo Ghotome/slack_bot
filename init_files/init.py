2cedaab1a5e91a1bc7b737fa051cb8cd7ed3b8f1
fishhead <egor@nefedov.org>
2021-11-09 12:58:51 +0200

    123

--- init_files/init.py
+++ init_files/init.py
@@ -15,7 +15,7 @@ def __init__():
 
             python = f"#!{path}/projects/slack_bot/slack_bot/bin/python3"
             os.chmod(f'{path}projects/slack_bot/main.py', 0o755)
-            print(subprocess.Popen((f'cat {path}project/slack_bot/init_files/slack_bot.service; '
+            print(subprocess.Popen((f'cat {path}projects/slack_bot/init_files/slack_bot.service; '
                                     'python3 -m venv slack_bot && '
                                     '. /home/fishhead/projects/slack_bot/slack_bot/bin/activate && '
                                     'pip install --upgrade pip && '
